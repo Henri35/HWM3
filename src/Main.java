660d93068b507336c2757894bfb2c7a516375dce
Henri35 <71730698+Henri35@users.noreply.github.com>
2021-08-12 20:45:49 +0200

    1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main
+
+
+
+{
+
+        public static void main(String[] args) {
+            /* write your code here */
+            System.out.println("Hello World");
+            System.out.println("How are you ?");
+            System.out.println("How are you2 ?");
+        }
+    }
+
+
+
